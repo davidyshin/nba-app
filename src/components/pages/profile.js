4f33ba43782184728ad863c11d24acb57cb1906b
davidyshin <davidshin@ac.c4q.nyc>
2017-12-20 08:58:35 -0500

    added more information to profile cards

--- src/components/pages/profile.js
+++ src/components/pages/profile.js
@@ -56,12 +56,15 @@ class Profile extends React.Component {
           {commonInfo.firstName} {commonInfo.lastName}{" "}
         </h1>
         <h3> Player ID : {commonInfo.personId} </h3>
+        <h3> Team : {commonInfo.teamCity} {commonInfo.teamName}</h3>
         <h3> Team ID : {commonInfo.teamId} </h3>
         <h3> Position : {commonInfo.position}</h3>
         <h3> Jersey # : {commonInfo.jersey} </h3>
-        <h2> PPG : {stats.pts}</h2>
-        <h2> RPG : {stats.reb}</h2>
-        <h2> APG : {stats.ast}</h2>
+        <h3> Weight : {commonInfo.weight}lb</h3>
+   
+        <p> PPG : {stats.pts}</p>
+        <p> RPG : {stats.reb}</p>
+        <p> APG : {stats.ast}</p>
         <img src={player.url} alt="player-headshot" />
         <br />
         <Link to="/players">Back</Link>
